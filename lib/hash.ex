57d8dbd15fdfe1365a7f64239f06077b8671e559
Wojtek Mach <wojtek@wojtekmach.pl>
2021-03-21 10:28:39 +0100

    Update Hash docs

--- lib/hash.ex
+++ lib/hash.ex
@@ -3,45 +3,28 @@ defmodule Hash do
   Functions for computing hashes.
   """
 
-  @spec sha256(binary() | Enumerable.t() | IO.device()) :: binary()
   @doc """
-  Computes a SHA256 hash of `input`.
-
-  Same as `hash(:sha256, input)`.
-  """
-  def sha256(input) do
-    hash(:sha256, input)
-  end
-
-  @doc """
-  Computes a hash of a binary, an enumerable, or an IO device.
+  Computes a hash of the input.
 
   ## Examples
 
-      iex> Hash.hash(:md5, "foo") |> Base.encode16()
-      "ACBD18DB4CC2F85CEDEF654FCCC4A4D8"
+      iex> ["foo\\n", "bar\\n"] |> Hash.hash(:md5) |> Base.encode16()
+      "F47C75614087A8DD938BA4ACFF252494"
 
-      iex> Hash.hash(:md5, ["f", "o", "o"]) |> Base.encode16()
-      "ACBD18DB4CC2F85CEDEF654FCCC4A4D8"
-
-      iex> StringIO.open("foo", [], fn pid ->
-      ...>   Hash.hash(:md5, pid) |> Base.encode16()
-      ...> end)
-      {:ok, "ACBD18DB4CC2F85CEDEF654FCCC4A4D8"}
+      iex> {:ok, pid} = StringIO.open("foo\\nbar\\n")
+      iex> stream = IO.stream(pid, :line)
+      iex> stream |> Hash.hash(:md5) |> Base.encode16()
+      "F47C75614087A8DD938BA4ACFF252494"
 
   """
-  @spec hash(:crypto.hash_algorithm(), binary() | Enumerable.t() | IO.device()) :: binary()
-  def hash(type, input)
+  @spec hash(iodata() | Enumerable.t(), :crypto.hash_algorithm()) :: binary()
+  def hash(iodata_or_enumerable, type)
 
-  def hash(type, binary) when is_binary(binary) do
-    :crypto.hash(type, binary)
+  def hash(iodata, type) when is_binary(iodata) or is_list(iodata) do
+    :crypto.hash(type, iodata)
   end
 
-  def hash(type, device) when is_atom(device) or is_pid(device) do
-    hash(type, IO.stream(device, 4 * 1024))
-  end
-
-  def hash(type, enumerable) do
+  def hash(enumerable, type) do
     enumerable
     |> Enum.reduce(:crypto.hash_init(type), &:crypto.hash_update(&2, &1))
     |> :crypto.hash_final()
